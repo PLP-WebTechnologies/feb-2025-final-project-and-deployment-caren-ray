9055882e8429c438181dce5d49acc218fdcea18d
caren-ray <rayoncaren@gmail.com>
2025-05-14 12:33:17 +0300

    completely redesigned the website to factor in responsiveness

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-// js/script.js
-// Image Slider
-let current = 0;
-const images = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg"];
-setInterval(() => {
-  const slider = document.getElementById("slider");
-  if (slider) {
-    slider.src = images[current];
-    current = (current + 1) % images.length;
-  }
-}, 3000);
-
-// Form Validation
-const form = document.getElementById("contactForm");
-if (form) {
-  form.addEventListener("submit", function (e) {
-    const email = document.getElementById("email").value;
-    if (!email.includes("@")) {
-      alert("Please enter a valid email address.");
-      e.preventDefault();
-    }
-  });
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-
